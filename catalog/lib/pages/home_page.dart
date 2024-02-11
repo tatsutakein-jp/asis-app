95646ad7fd8df6cd21c556f87cb16feade8ed489
Ryo Takeuchi <develop@tatsutakein.jp>
2024-02-11 20:21:10 +0900

    fix(catalog): camel -> Pascal

--- catalog/lib/pages/home_page.dart
+++ catalog/lib/pages/home_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 
 @widgetbook.UseCase(
-  name: 'homePage',
+  name: 'HomePage',
   type: HomePage,
   path: '[Pages]',
 )
