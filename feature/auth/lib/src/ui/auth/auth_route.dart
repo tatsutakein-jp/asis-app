1da83bcd3524aeeef4a8af6e12d374f1e3cfabda
Ryo Takeuchi <develop@tatsutakein.jp>
2024-02-13 21:23:18 +0900

    feat(quest): AuthRouteData を追加

---
+++ feature/auth/lib/src/ui/auth/auth_route.dart
@@ -0,0 +1,10 @@
+import 'package:feature_auth/src/ui/auth/auth_page.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+abstract class AuthRouteData extends GoRouteData {
+  const AuthRouteData();
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) => AuthPage();
+}
