eaeb31eef6ad310f2657a8636c3ad64a8d7d5faa
Daichi Aoki <fremie.off@gmail.com>
2024-02-22 18:44:33 +0900

    feat: authPageのボタン遷移用Providerを作成

---
+++ feature/auth/lib/src/ui/auth/auth_route_args.dart
@@ -0,0 +1,7 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'auth_route_args.g.dart';
+
+@riverpod
+void Function() onTappedButton(OnTappedButtonRef ref) =>
+    throw UnimplementedError('Auth_onTappedButton');
---
+++ feature/auth/lib/src/ui/auth/auth_route_args.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'auth_route_args.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$onTappedButtonHash() => r'e3d9f269e19909e788b182944a48cb461a4edd79';
+
+/// See also [onTappedButton].
+@ProviderFor(onTappedButton)
+final onTappedButtonProvider = AutoDisposeProvider<void Function()>.internal(
+  onTappedButton,
+  name: r'onTappedButtonProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$onTappedButtonHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef OnTappedButtonRef = AutoDisposeProviderRef<void Function()>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
