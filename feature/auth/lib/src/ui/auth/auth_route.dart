1eaa9fcb316042e1806f554549af6cc29ee0ab5c
Daichi Aoki <fremie.off@gmail.com>
2024-02-22 18:45:35 +0900

    feat: AuthRouteDataでボタンアクションをDI出来るよう修正

--- feature/auth/lib/src/ui/auth/auth_route.dart
+++ feature/auth/lib/src/ui/auth/auth_route.dart
@@ -1,10 +1,26 @@
 import 'package:feature_auth/src/ui/auth/auth_page.dart';
+import 'package:feature_auth/src/ui/auth/auth_route_args.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 abstract class AuthRouteData extends GoRouteData {
   const AuthRouteData();
 
+  abstract final OnTappedButtonCallback onTappedButton;
+
+  @nonVirtual
   @override
-  Widget build(BuildContext context, GoRouterState state) => AuthPage();
+  Widget build(BuildContext context, GoRouterState state) => ProviderScope(
+        overrides: [
+          onTappedButtonProvider
+              .overrideWith((ref) => () => onTappedButton(context)),
+        ],
+        child: AuthPage(),
+      );
 }
+
+typedef OnTappedButtonCallback = void Function(
+  BuildContext context,
+);
