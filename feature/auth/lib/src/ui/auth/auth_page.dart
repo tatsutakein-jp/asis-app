f9a63a18b10b4e8b2ade0fe9c679ed44fe8b2c80
Daichi Aoki <fremie.off@gmail.com>
2024-02-22 18:44:56 +0900

    feat: AuthPageに適当なボタンを配置

--- feature/auth/lib/src/ui/auth/auth_page.dart
+++ feature/auth/lib/src/ui/auth/auth_page.dart
@@ -1,5 +1,7 @@
 import 'package:core_designsystem/component.dart';
+import 'package:feature_auth/src/ui/auth/auth_route_args.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
@@ -13,7 +15,14 @@ final class AuthPage extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
-            Text('Auth Page'),
+            Consumer(
+              builder: (context, ref, child) {
+                return TextButton(
+                  onPressed: ref.read(onTappedButtonProvider),
+                  child: const Text('Auth Page'),
+                );
+              },
+            ),
           ],
         ),
       ),
