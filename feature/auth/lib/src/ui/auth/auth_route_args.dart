25722e7714c4006a29b931fa2f9db29d1dbe8cbf
Daichi Aoki <fremie.off@gmail.com>
2024-02-22 20:36:47 +0900

    refactor: AuthPageのボタンアクションのProvider削除

--- feature/auth/lib/src/ui/auth/auth_route_args.dart
+++
@@ -1,7 +0,0 @@
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-
-part 'auth_route_args.g.dart';
-
-@riverpod
-void Function() onTappedButton(OnTappedButtonRef ref) =>
-    throw UnimplementedError('Auth_onTappedButton');
