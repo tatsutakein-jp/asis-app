3addeb72de36ab02cf7842496af3c9731cdab9ab
Ryo Takeuchi <develop@tatsutakein.jp>
2024-02-26 12:31:03 +0900

    feat(app): ライセンスページへの遷移を追加

--- app/lib/router/go_route/settings_route.dart
+++ app/lib/router/go_route/settings_route.dart
@@ -1,4 +1,5 @@
 import 'package:asis_app/router/app_page_path.dart';
+import 'package:asis_app/router/shell_route/app_shell_route.dart';
 import 'package:feature_settings/feature_settings.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -8,12 +9,26 @@ part 'settings_route.g.dart';
 /// TypedGoRoute for feature_settings
 ///
 /// セッティングタブ内でのpush先はここのroutesにネストしていく
-const settingsRoute = TypedGoRoute<SettingsRoute>(path: AppPagePath.settings);
+const settingsRoute = TypedGoRoute<SettingsRoute>(
+  path: AppPagePath.settings,
+  routes: [
+    TypedGoRoute<LicenseRoute>(
+      path: AppPagePath.license,
+    ),
+  ],
+);
 
 @TypedGoRoute<SettingsRoute>(path: AppPagePath.settings)
 final class SettingsRoute extends SettingsRouteData {
   const SettingsRoute();
 
   @override
-  OnTapOpenSourceLicenseCallback get onTapOpenSourceLicense => (context) {};
+  OnTapOpenSourceLicenseCallback get onTapOpenSourceLicense => (context) {
+        const LicenseRoute().go(context);
+      };
+}
+
+/// ライセンスページへのルート
+final class LicenseRoute extends LicenseRouteData {
+  const LicenseRoute();
 }
