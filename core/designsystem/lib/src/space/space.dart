44246165b4ae9f20615dd20d050fca54f2ed3184
Ryo Takeuchi <develop@tatsutakein.jp>
2024-04-12 20:20:33 +0900

    feat(designsystem): スペースの定義を追加

---
+++ core/designsystem/lib/src/space/space.dart
@@ -0,0 +1,13 @@
+final class AsisSpace {
+  AsisSpace._();
+
+  static const int x3s = 2;
+  static const int x2s = 4;
+  static const int xs = 6;
+  static const int s = 8;
+  static const int m = 16;
+  static const int l = 24;
+  static const int xl = 32;
+  static const int x2l = 40;
+  static const int x3l = 48;
+}
