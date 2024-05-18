b13b980d1a8d553cb43f3a324ab50436d3234bc5
Ryo Takeuchi <develop@tatsutakein.jp>
2024-05-18 18:03:05 +0900

    feat(domain): 認証系のユースケースを追加

---
+++ core/domain/lib/auth_use_case.dart
@@ -0,0 +1,2 @@
+export 'src/use_case/auth/sign_in_use_case.dart';
+export 'src/use_case/auth/sign_out_use_case.dart';
---
+++ core/domain/lib/src/use_case/auth/sign_in_use_case.dart
@@ -0,0 +1,16 @@
+import 'package:core_authenticator/authenticator.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'sign_in_use_case.g.dart';
+
+/// サインインする ユースケース
+@riverpod
+Future<void> signInUseCase(
+  SignInUseCaseRef ref, {
+  required String email,
+  required String password,
+}) async =>
+    ref.watch(authenticatorProvider).signInWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
