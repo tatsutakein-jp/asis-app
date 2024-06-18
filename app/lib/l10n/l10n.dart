02a0cd2929f7e3d2ef18436ede2d9d8c2a82692f
Ryo Takeuchi <develop@tatsutakein.jp>
2024-06-18 18:32:02 +0900

    refactor: use typedef

--- app/lib/l10n/l10n.dart
+++ app/lib/l10n/l10n.dart
@@ -1,27 +1,27 @@
 import 'package:app/gen/l10n/l10n.dart';
-import 'package:core_ui/l10n.dart' as core_ui;
-import 'package:feature_auth/feature_auth.dart' as feature_auth;
-import 'package:feature_feed/feature_feed.dart' as feature_feed;
-import 'package:feature_home/feature_home.dart' as feature_home;
-import 'package:feature_quest/feature_quest.dart' as feature_quest;
-import 'package:feature_settings/feature_settings.dart' as feature_settings;
+import 'package:core_ui/l10n.dart';
+import 'package:feature_auth/feature_auth.dart';
+import 'package:feature_feed/feature_feed.dart';
+import 'package:feature_home/feature_home.dart';
+import 'package:feature_quest/feature_quest.dart';
+import 'package:feature_settings/feature_settings.dart';
 
 const appLocalizationsDelegates = [
   ...L10n.localizationsDelegates,
-  ...core_ui.L10n.localizationsDelegates,
-  ...feature_auth.L10n.localizationsDelegates,
-  ...feature_feed.L10n.localizationsDelegates,
-  ...feature_home.L10n.localizationsDelegates,
-  ...feature_quest.L10n.localizationsDelegates,
-  ...feature_settings.L10n.localizationsDelegates,
+  ...CoreUiL10n.localizationsDelegates,
+  ...FeatureAuthL10n.localizationsDelegates,
+  ...FeatureFeedL10n.localizationsDelegates,
+  ...FeatureHomeL10n.localizationsDelegates,
+  ...FeatureQuestL10n.localizationsDelegates,
+  ...FeatureSettingsL10n.localizationsDelegates,
 ];
 
 const appSupportedLocales = [
   ...L10n.supportedLocales,
-  ...core_ui.L10n.supportedLocales,
-  ...feature_auth.L10n.supportedLocales,
-  ...feature_feed.L10n.supportedLocales,
-  ...feature_home.L10n.supportedLocales,
-  ...feature_quest.L10n.supportedLocales,
-  ...feature_settings.L10n.supportedLocales,
+  ...CoreUiL10n.supportedLocales,
+  ...FeatureAuthL10n.supportedLocales,
+  ...FeatureFeedL10n.supportedLocales,
+  ...FeatureHomeL10n.supportedLocales,
+  ...FeatureQuestL10n.supportedLocales,
+  ...FeatureSettingsL10n.supportedLocales,
 ];
