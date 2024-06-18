9de7a840e58f93ac38c38c854113e69caca9da94
Ryo Takeuchi <develop@tatsutakein.jp>
2024-06-18 23:05:11 +0900

    fix: ビルド時のエラーを修正

--- core/network_ferry/lib/src/graphql/__generated__/schema.schema.gql.dart
+++ core/network_ferry/lib/src/graphql/__generated__/schema.schema.gql.dart
@@ -7,7 +7,7 @@ import 'package:built_value/built_value.dart';
 import 'package:built_value/serializer.dart';
 import 'package:core_network_ferry/src/graphql/__generated__/serializers.gql.dart'
     as _i1;
-import 'package:gql_code_builder/src/serializers/default_scalar_serializer.dart'
+import 'package:gql_code_builder_serializers/gql_code_builder_serializers.dart'
     as _i2;
 
 part 'schema.schema.gql.g.dart';
