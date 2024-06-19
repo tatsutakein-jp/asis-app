57953790e260dd18c09f664536899405e2c1f223
Ryo Takeuchi <develop@tatsutakein.jp>
2024-06-19 21:18:38 +0900

    feat(designsystem): ブランドアイコンを追加

--- core/designsystem/lib/src/component/index.dart
+++ core/designsystem/lib/src/component/index.dart
@@ -1,3 +1,4 @@
 export 'app_bar/index.dart';
 export 'card/index.dart';
+export 'icon/index.dart';
 export 'scaffold/index.dart';
---
+++ core/designsystem/lib/src/gen/assets/assets.gen.dart
@@ -0,0 +1,27 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/brand_icon.svg
+  String get brandIcon =>
+      'packages/core_designsystem/assets/images/brand_icon.svg';
+
+  /// List of all assets
+  List<String> get values => [brandIcon];
+}
+
+class Assets {
+  Assets._();
+
+  static const String package = 'core_designsystem';
+
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
