86befcf2fbf63da9d1b0f84793d02fb30eda44bf
Ryo Takeuchi <develop@tatsutakein.jp>
2024-07-19 14:19:23 +0900

    ci: git 管理対象外ファイル生成のパスを修正

--- tool/gen_gitignore_files.sh
+++ tool/gen_gitignore_files.sh
@@ -16,10 +16,10 @@ echo ""
 echo "🚀 Firebase Options: Start"
 
 # source file (dev)
-FIREBASE_OPTIONS_SOURCE_FILE="app/lib/flavor/dev/firebase_options.dart"
+FIREBASE_OPTIONS_SOURCE_FILE="app/mobile/lib/flavor/dev/firebase_options.dart"
 
 # copy to stg
-FIREBASE_OPTIONS_TARGET_STG="app/lib/flavor/stg/firebase_options.dart"
+FIREBASE_OPTIONS_TARGET_STG="app/mobile/lib/flavor/stg/firebase_options.dart"
 cp -i "$FIREBASE_OPTIONS_SOURCE_FILE" "$FIREBASE_OPTIONS_TARGET_STG"
 FIREBASE_OPTIONS_COPY_TO_STG=$?
 if [ $FIREBASE_OPTIONS_COPY_TO_STG -eq 0 ]; then
@@ -29,7 +29,7 @@ else
 fi
 
 # copy to prod
-FIREBASE_OPTIONS_TARGET_PROD="app/lib/flavor/prod/firebase_options.dart"
+FIREBASE_OPTIONS_TARGET_PROD="app/mobile/lib/flavor/prod/firebase_options.dart"
 cp -i "$FIREBASE_OPTIONS_SOURCE_FILE" "$FIREBASE_OPTIONS_TARGET_PROD"
 FIREBASE_OPTIONS_COPY_TO_PROD=$?
 if [ $FIREBASE_OPTIONS_COPY_TO_PROD -eq 0 ]; then
@@ -54,10 +54,10 @@ echo ""
 echo "🚀 Env: Start"
 
 # source file (sample)
-ENV_SOURCE_FILE="app/.env.sample"
+ENV_SOURCE_FILE="app/mobile/.env.sample"
 
 # copy
-ENV_TARGET="app/.env"
+ENV_TARGET="app/mobile/.env"
 cp -i "$ENV_SOURCE_FILE" "$ENV_TARGET"
 ENV_COPY_TO_TARGET=$?
 if [ $ENV_COPY_TO_TARGET -eq 0 ]; then
