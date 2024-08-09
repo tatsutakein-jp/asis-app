ed76d76ae6583e3ad9dd7b0d440b89a6fe1a9273
Ryo Takeuchi <develop@tatsutakein.jp>
2024-08-10 08:22:06 +0900

    feat(domain): クエスト数を取得するユースケースを追加

---
+++ core/domain/lib/src/use_case/quest/quest_count_stream_use_case.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'quest_count_stream_use_case.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$questCountStreamUseCaseHash() =>
+    r'35cf2ab9d30f1d9e4ea86d0876cd298781e8bd5f';
+
+/// クエスト数を取得する ユースケース
+///
+/// Copied from [questCountStreamUseCase].
+@ProviderFor(questCountStreamUseCase)
+final questCountStreamUseCaseProvider =
+    AutoDisposeStreamProvider<QuestCount>.internal(
+  questCountStreamUseCase,
+  name: r'questCountStreamUseCaseProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$questCountStreamUseCaseHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef QuestCountStreamUseCaseRef = AutoDisposeStreamProviderRef<QuestCount>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
