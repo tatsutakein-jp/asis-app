6d571ca51067ddfd870f5359f1484a914e2db930
Ryo Takeuchi <develop@tatsutakein.jp>
2024-10-23 08:46:40 +0900

    fix: generated files

--- core/domain/lib/src/use_case/feed/feed_stream_by_id_use_case.g.dart
+++ core/domain/lib/src/use_case/feed/feed_stream_by_id_use_case.g.dart
@@ -155,6 +155,8 @@ class FeedStreamByIdUseCaseProvider extends AutoDisposeStreamProvider<Feed?> {
   }
 }
 
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
 mixin FeedStreamByIdUseCaseRef on AutoDisposeStreamProviderRef<Feed?> {
   /// The parameter `id` of this provider.
   String get id;
@@ -169,4 +171,4 @@ class _FeedStreamByIdUseCaseProviderElement
   String get id => (origin as FeedStreamByIdUseCaseProvider).id;
 }
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
